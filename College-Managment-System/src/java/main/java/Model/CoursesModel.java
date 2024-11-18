489003a1ebc22be1b4f6ac927098b51f4e155dc7
JCCJR0517 <jc574997@gmail.com>
2024-11-18 06:34:00 -0600

    Create CoursesModel.java

---
+++ College-Managment-System/src/java/main/java/Model/CoursesModel.java
@@ -0,0 +1,25 @@
+@Entity
+@Table(name = "courses")
+public class Course {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String code;
+
+    @ManyToOne
+    @JoinColumn(name = "faculty_id")
+    private Faculty faculty;
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
