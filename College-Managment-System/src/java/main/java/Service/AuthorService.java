7157435b360830d62da9076daeb06158921898c8
JCCJR0517 <jc574997@gmail.com>
2024-11-18 06:44:28 -0600

    UserService.java

--- College-Managment-System/src/java/main/java/Service/AuthorService.java
+++ College-Managment-System/src/java/main/java/Service/AuthorService.java
@@ -1,22 +1,30 @@
-package com.example.collegemanagement.service;
-
-import com.example.collegemanagement.model.User;
-import com.example.collegemanagement.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 @Service
-public class AuthService {
+public class UserService {
+
     @Autowired
     private UserRepository userRepository;
 
-    public User authenticate(String username, String password) {
-        User user = userRepository.findByUsername(username);
-        if (user != null && user.getPasswordHash().equals(password)) {
-            return user;
-        }
-        return null;
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public User getUserById(Long id) {
+        return userRepository.findById(id).orElseThrow(() -> new RuntimeException("User not found"));
+    }
+
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public User updateUser(Long id, User updatedUser) {
+        User user = getUserById(id);
+        user.setUsername(updatedUser.getUsername());
+        user.setPassword(updatedUser.getPassword());
+        user.setRole(updatedUser.getRole());
+        return userRepository.save(user);
+    }
+
+    public void deleteUser(Long id) {
+        userRepository.deleteById(id);
     }
 }
-
-
